cd30fff0e8ad72fd255aff4b47e4d4d4d2bf7adb
diegoaraujo85 <doasalah@gmail.com>
2020-02-11 08:29:02 -0300

    alterando estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
 
-class TechList extends Component{
-state={
-  techs:[
-    'NodeJS','ReactJS','React Native'
-  ]
-}
+class TechList extends Component {
+
+  state = {
+    newTech : '',
+    techs: ['Nodejs', 'React','React Native']
+  };
+
+  handleInputChange = e => { //precisa ser arrow function para poder ter acesso ao this
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+
+    this.setState({
+      techs:[...this.state.techs, this.state.newTech]
+    })
+  }
 
   render(){
     return (
-      <ul>
-        <li>NodeJS</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li> )}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
   }
 }
 
