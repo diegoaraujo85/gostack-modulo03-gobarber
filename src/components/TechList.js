2174afc8dac7012eb1b40863b914ef8425b13b91
diegoaraujo85 <doasalah@gmail.com>
2020-02-11 09:26:12 -0300

    removendo itens do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,29 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class TechList extends Component {
-
   state = {
-    newTech : '',
-    techs: ['Nodejs', 'React','React Native']
+    newTech: '',
+    techs: ['Nodejs', 'React', 'React Native'],
   };
 
-  handleInputChange = e => { //precisa ser arrow function para poder ter acesso ao this
+  handleInputChange = e => {
+    //precisa ser arrow function para poder ter acesso ao this
     this.setState({ newTech: e.target.value });
-  }
+  };
 
-  handleSubmit = e =>{
+  handleSubmit = e => {
     e.preventDefault();
 
     this.setState({
-      techs:[...this.state.techs, this.state.newTech]
-    })
-  }
+      techs: [...this.state.techs, this.state.newTech],
+    });
 
-  render(){
+    this.state.newTech = '';
+  };
+
+  handleDelete = tech => {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech),
+    });
+  };
+
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li> )}
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>
+          ))}
         </ul>
         <input
           type="text"
